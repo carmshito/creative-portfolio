941844009a837692a46a84ef55a213d9db270460
Carmelia Shito <carmshito@hotmail.com>
2024-01-08 15:25:41 -0500

    remove project related components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
 import Intro from './components/Intro';
 import About from './components/About';
-import ProjectsList from './components/ProjectsList';
 
 function App() {
   return (
     <div>
      <Intro />
      <About />
-     <ProjectsList />
     </div>
   );
 }
