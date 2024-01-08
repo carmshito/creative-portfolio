6073a920b582ddc3e448e23ec4f43d477e698455
Carmelia Shito <carmshito@hotmail.com>
2024-01-08 18:46:13 -0500

    refactor to move data to data.js

--- src/data.js
+++ src/data.js
@@ -1,6 +1,102 @@
+////////////// mobile img ////////////////
+import BarkMingleImg from "./assets/projects/bm-landing-screen.png";
+
+////////////// web img ////////////////
+import FunsiesImg from "./assets/projects/funsies-dropdown.png";
+import JungleImg from "./assets/projects/jungle-rails-landing.png";
+import PhotoLabsImg from "./assets/projects/photolabs.png";
+
+////////////// illustration img ////////////////
+import BenjiImg from "./assets/illustrations/benji.png";
+import CinnyOscar from "./assets/illustrations/cinny-oscar.png";
+import ChareneImg from "./assets/illustrations/charene.png";
+import FurbendersImg from "./assets/illustrations/furbenders.png";
+import LynnImg from "./assets/illustrations/lynn.png";
+import MaddyImg from "./assets/illustrations/maddy.png";
+import MotherImg from "./assets/illustrations/mother.png";
+import KobeImg from "./assets/illustrations/kobe.png";
+import WianeImg from "./assets/illustrations/wiane.png";
+import JessImg from "./assets/illustrations/jess-yuzu.png";
+import MegImg from "./assets/illustrations/meg.png";
+import GeraldDaniImg from "./assets/illustrations/gerald-dani.png";
+
+export const webProjects = [
+  {
+    id: 1,
+    name: "Funsies",
+    img: FunsiesImg,
+    link: "https://github.com/carmshito/Funsies",
+  },
+  {
+    id: 2,
+    name: "Jungle Rails",
+    img: JungleImg,
+    link: "https://github.com/carmshito/jungle-rails",
+  },
+  {
+    id: 3,
+    name: "PhotoLabs",
+    img: PhotoLabsImg,
+    link: "https://github.com/carmshito/photolabs",
+  },
+];
+
+export const mobileProjects = [
+  {
+    id: 1,
+    name: "Bark Mingle",
+    img: BarkMingleImg,
+    link: "https://github.com/carmshito/BarkMingle.git",
+  },
+]
+
 export const illustrations = [
   {
     id: 1,
-    img: "",
+    img: GeraldDaniImg,
+  },
+  {
+    id: 1,
+    img: MegImg,
+  },
+  {
+    id: 1,
+    img: JessImg,
+  },
+  {
+    id: 1,
+    img: BenjiImg
+  },
+  {
+    id: 2,
+    img: CinnyOscar
+  },
+  {
+    id: 3,
+    img: FurbendersImg
+  },
+  {
+    id: 4,
+    img: LynnImg
+  },
+  {
+    id: 5,
+    img: WianeImg
+  },
+  {
+    id: 6,
+    img: MaddyImg
+  },
+  {
+    id: 7,
+    img: ChareneImg
+  },
+  {
+    id: 8,
+    img: KobeImg,
+  },
+  {
+    id: 9,
+    img: MotherImg,
   },
 ]
