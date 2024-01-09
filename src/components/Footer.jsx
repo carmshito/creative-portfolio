8c98e7fb975ff2e1fed3a6cf35922edfbc9c33ce
Carmelia Shito <carmshito@hotmail.com>
2024-01-09 17:59:08 -0500

    add social icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,21 @@
-import React from "react"
+import React from "react";
+import "../styles/Footer.scss";
 import { Col, Container, Row } from "react-bootstrap";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
     <footer className="footer">
       <Container>
         <Row className="align-item-center">
-          <Col sm={6}>
+          <Col size={12} sm={6}>
           </Col>
-          <Col sm={6}>
+          <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href=""></a>
+              <a href="https://www.linkedin.com/in/carmelia-shito/"><FaLinkedin className="icon"/></a>
+              <a href="https://github.com/carmshito"><FaGithubSquare className="icon"/></a>
             </div>
           </Col>
         </Row>
