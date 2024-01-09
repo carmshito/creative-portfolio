ca83ff067ae20768625f5194e98a2ff632d1ae98
Carmelia Shito <carmshito@hotmail.com>
2024-01-09 17:07:29 -0500

    add footer component, install react-icons library

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Col, Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-item-center">
+          <Col sm={6}>
+          </Col>
+          <Col sm={6}>
+            <div className="social-icon">
+              <a href=""></a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
